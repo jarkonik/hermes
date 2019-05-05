5cec0b7a88efa7c7b87ad482e5da0fe053370fdd
Jaroslaw Konik <konikjar@gmail.com>
2019-05-05 11:31:36 +0200

    Sending msg ui

---
+++ hermes/Conversation.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hermes
+{
+    /// <summary>
+    /// Interaction logic for Conversation.xaml
+    /// </summary>
+    public partial class Conversation : UserControl
+    {
+        readonly ConversationModel model = new ConversationModel();
+
+        public Conversation()
+        {
+            InitializeComponent();
+            model.Messages.Add("Hi");
+            model.Messages.Add("Hi");
+            model.Messages.Add("What's up?");
+            model.Messages.Add("Nothing");
+
+            this.DataContext = model;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            model.SendMessage();
+        }
+    }
+}
---
+++ hermes/ConversationModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace hermes
+{
+    class ConversationModel : INotifyPropertyChanged
+    {
+        private string messageContent;
+
+        public string MessageContent {
+            get { return messageContent; }
+            set { messageContent = value; PropertyChanged(this, new PropertyChangedEventArgs("MessageContent")); }
+        }
+        public List<string> Messages { get; set; } = new List<string>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void SendMessage()
+        {
+            System.Console.WriteLine(MessageContent);
+            Messages.Add(MessageContent);
+            MessageContent = "";
+        }
+    }
+}
