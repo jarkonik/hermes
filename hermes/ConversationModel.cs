3a94fff4d5399f61da02e2deb33776455125794f
Jaroslaw Konik <konikjar@gmail.com>
2019-05-05 12:00:06 +0200

    Sending msgs

--- hermes/ConversationModel.cs
+++ hermes/ConversationModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -11,18 +12,60 @@ namespace hermes
     class ConversationModel : INotifyPropertyChanged
     {
         private string messageContent;
+        private FBClient client = new FBClient();
+
+        private ObservableCollection<string> messages = new ObservableCollection<string>();
 
         public string MessageContent {
             get { return messageContent; }
             set { messageContent = value; PropertyChanged(this, new PropertyChangedEventArgs("MessageContent")); }
         }
-        public List<string> Messages { get; set; } = new List<string>();
+
+        public ObservableCollection<string> Messages
+        {
+            get
+            {
+                return messages;
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        internal void SendMessage()
+        private async void InitFB()
+        {
+            var logged_in = await client.DoLogin("xxx", "xxx");
+
+            if (logged_in)
+            {
+                var users = await client.fetchAllUsers();
+                foreach (var user in users)
+                {
+                    Console.WriteLine("{0} {1}", user.name, user.uid);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error logging in");
+            }
+        }
+
+        ~ConversationModel()
+        {
+            client.DoLogout();
+        }
+
+        public ConversationModel()
+        {
+            InitFB();
+        }
+
+
+
+        internal async void SendMessage()
         {
             System.Console.WriteLine(MessageContent);
+            await client.SendMessage(MessageContent, thread_id: "100000672971086");
+            
             Messages.Add(MessageContent);
             MessageContent = "";
         }
